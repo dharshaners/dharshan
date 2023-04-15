4b068793aa8c9d5b735f24c9a1f515076123d936
dharshaners <dharshaners@gmail.com>
2023-04-15 10:23:58 +0530

    first commit

--- pipeline/jenkinspipeline/jenkins/build/mvn.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "***************************"
-echo "** Building jar ***********"
-echo "***************************"
-
-WORKSPACE=/home/ec2-user/jenkins_home/workspace/demo
-
-docker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
