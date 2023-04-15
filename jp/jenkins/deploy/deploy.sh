521e487c46b0906bd315de95426b9b98b3bca50b
dharshaners <dharshaners@gmail.com>
2023-04-15 16:46:41 +0530

    first commit

--- jp/jenkins/deploy/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo maven-project > /tmp/.auth
-echo $BUILD_TAG >> /tmp/.auth
-echo $PASS >> /tmp/.auth
-
-scp -i /opt/prod /tmp/.auth ec2-user@35.174.184.95:/tmp/.auth
--- jp/jenkins/test/mvn.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "***************************"
-echo "** Testing the code ***********"
-echo "***************************"
-WORKSPACE=/home/ec2-user/jenkins_home/workspace/demo
-
-docker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
